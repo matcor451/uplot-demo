589b40d87597255f4c5baf02cfb5f9fa3c07a986
Matt McCormack <matthewmccormack@gmail.com>
2025-05-11 09:57:20 +0100

    move keyhandler to new file

--- src/app/components/Chart.tsx
+++ src/app/components/Chart.tsx
@@ -7,6 +7,7 @@ import { seriesFromData } from './utils';
 import { seriesPointsPlugin } from './plugins';
 import { Flag } from '../page';
 import { Button } from './Button';
+import { onKeyDown } from './eventHandlers';
 
 interface Props {
   data: number[][]
@@ -18,23 +19,7 @@ const initHook = (u: uPlot) => {
 
   u.over.addEventListener(
     "keydown",
-    (e) => {
-      // const xStep = 0.5
-      const xStep = (u.scales.x.max! - u.scales.x.min!) * 0.1
-      // const yStep = 2
-      const yStep = (u.scales['y'].max! - u.scales['y'].min!) * 0.1
-      if (e.key === "ArrowLeft") {
-        u.setScale('x', {min: u.scales.x.min! - xStep, max: u.scales.x.max! - xStep})
-        u.setScale('y', {min: u.scales.y.min!, max: u.scales.y.max!})
-      } else if (e.key === "ArrowRight") {
-        u.setScale('x', {min: u.scales.x.min! + xStep, max: u.scales.x.max! + xStep})
-        u.setScale('y', {min: u.scales.y.min!, max: u.scales.y.max!})
-      } else if (e.key === 'ArrowUp') {
-        u.setScale('y', {min: u.scales.y.min! + yStep, max: u.scales.y.max! + yStep})
-      } else if (e.key === 'ArrowDown') {
-        u.setScale('y', {min: u.scales.y.min! - yStep, max: u.scales.y.max! - yStep})
-      }
-    },
+    onKeyDown(u),
     true
   );
 }
---
+++ src/app/components/eventHandlers.ts
@@ -0,0 +1,19 @@
+import uPlot from "uplot"
+
+export const onKeyDown = (u: uPlot) => (e: KeyboardEvent) => {
+  // const xStep = 0.5
+  const xStep = (u.scales.x.max! - u.scales.x.min!) * 0.1
+  // const yStep = 2
+  const yStep = (u.scales['y'].max! - u.scales['y'].min!) * 0.1
+  if (e.key === "ArrowLeft") {
+    u.setScale('x', {min: u.scales.x.min! - xStep, max: u.scales.x.max! - xStep})
+    u.setScale('y', {min: u.scales.y.min!, max: u.scales.y.max!})
+  } else if (e.key === "ArrowRight") {
+    u.setScale('x', {min: u.scales.x.min! + xStep, max: u.scales.x.max! + xStep})
+    u.setScale('y', {min: u.scales.y.min!, max: u.scales.y.max!})
+  } else if (e.key === 'ArrowUp') {
+    u.setScale('y', {min: u.scales.y.min! + yStep, max: u.scales.y.max! + yStep})
+  } else if (e.key === 'ArrowDown') {
+    u.setScale('y', {min: u.scales.y.min! - yStep, max: u.scales.y.max! - yStep})
+  }
+}
